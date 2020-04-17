fd1e5da12b2724823141f1ca80c409bcebe74e5c
Chen xingyu <2500941316@qq.com>
2020-04-17 16:41:43 +0800

    0417修改

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -31,8 +31,15 @@ const webpackConfig = merge(baseWebpackConfig, {
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env,
+<<<<<<< Updated upstream
       // 'apiPath':"'//114.55.93.118:8080'",
       'apiPath':"'//202.120.117.43:8080'",
+=======
+
+      'apiPath':"'//202.120.117.43:8443'",
+      'apiPath2':"'//202.120.117.43:8080'",
+
+>>>>>>> Stashed changes
     }),
     new UglifyJsPlugin({
       uglifyOptions: {
